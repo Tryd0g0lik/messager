b0407e16cf2732af5dfa19d63d0e50c590a3a5d3
Tryd0g0lik <work80@mail.ru>
2024-04-06 10:39:25 +0700

    pause

--- app_messager/models.py
+++ app_messager/models.py
@@ -1,8 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-class AdvUser(AbstractUser):
-	pass
+from django.contrib.auth.models import AbstractUser, User, Group
 
-	class Meta(AbstractUser.Meta):
-		pass
+
+# # Create your models here.
+# class AdvUser(AbstractUser):
+# 	user = models.ForeignKey(
+# 		User,
+# 		on_delete=models.CASCADE,
+# 		related_name="myUser",
+# 	)
+# 	groups = models.ForeignKey(
+# 		Group,
+# 		on_delete=models.CASCADE,
+# 		related_name="myGroup",
+# 	)
+# 	user_permissions =models.ForeignKey(
+# 		Group,
+# 		on_delete=models.CASCADE,
+# 		related_name="myGroup",
+# 	)
+# 	class Meta(AbstractUser.Meta):
+# 		pass
