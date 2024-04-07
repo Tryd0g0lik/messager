d4a14336e8765eb54cc27e09fed0ee8a84fa7804
Tryd0g0lik <work80@mail.ru>
2024-04-07 12:21:38 +0700

    [master]: pouse

--- app_messager/views.py
+++ app_messager/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+import websocket, json
 
 # Create your views here.
 def home_page(request):
+	# ws = websocket.WebSocket()
+	# ws.connect('ws://localhost:6379/ws/tableData/')
 	return render(request, 'index.html', {})
--- project/returning.py
+++ project/returning.py
@@ -4,17 +4,23 @@ from channels.security.websocket import AllowedHostsOriginValidator
 # from django.core.asgi import get_asgi_application
 from channels.security.websocket import OriginValidator
 from django.urls import path
+from app_messager import consummer
+import os
+from django.core.asgi import get_asgi_application
 
+ws_pattern = [
+	path("ws",consummer.TableData),
+]
 
+# router
 application =  ProtocolTypeRouter(
 	{
 		# "http": django_asgi_app,
-		"websocket": AllowedHostsOriginValidator(
+		"websocket": AuthMiddlewareStack( #  AllowedHostsOriginValidator(
 			# There to connecting websocket
-			AuthMiddlewareStack(URLRouter(  [ # https://youtu.be/XcV09pJ4upU?t=672
-				# path("", AdminChatConsumer.as_asgi()), # path: r"^longo/$"
-				],))  #
-['127.0.1', "http://custom-tools.online/" ]
-		),
+
+				URLRouter( ws_pattern ) # https://youtu.be/r6oTcAYDRt0?t=590 https://youtu.be/XcV09pJ4upU?t=672
+ 			),  # ['127.0.1', "http://custom-tools.online/" ]
+		#),
 	}
 )
