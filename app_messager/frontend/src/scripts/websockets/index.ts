18a0be27bb6fc6b841ff58e3072f5bf0ce27c7c7
Tryd0g0lik <work80@mail.ru>
2024-04-08 17:32:33 +0700

    [1]: JS has been added  the WServer

--- app_messager/frontend/src/scripts/websockets/index.ts
+++ app_messager/frontend/src/scripts/websockets/index.ts
@@ -12,23 +12,29 @@
  */
 export class WSocket {
   socket: any;
-  handlers: any;
-  // url: string;
+
+  handlers: {
+    open: []
+    close: []
+    data: []
+  };
+
   constructor(url: string) {
     this.socket = new WebSocket(url);
-    console.log("[websokets: WS-constructor URL]: ", url);
+
     this.socket.addEventListener('open', (e: any) => { console.log('OPEN') });
+
     this.socket.addEventListener('message', (e: any) => {
       console.log(`[websokets > message]: WebSocket - message was received; E.TARGET.URL ${e.target.url}, ${e.code}`);
 
       this.onMessage(e);
     });
+
     this.socket.addEventListener('close', (e: any) => {
       // if (e.wasClean) { console.log('[websokets > close] : connection was breaked]') }
       // else { console.log('[websokets > close]: - connection was closed]') };
-      console.log(`[websokets > close]: - connection was closed. ${e}`)
+      console.log(`[websokets > close]: - connection was closed. ${e}`);
       // console.log('[websokets: WebSocket.addEventListener("close") - closed Event]: ', e['message']);
-
     });
     this.socket.addEventListener('error', (e: any) => { });
 
@@ -39,36 +45,40 @@ export class WSocket {
     };
   }
 
-  sends(datas: string) { this.handlers.data.push(datas) };
-  onOpen() {
+  sends(datas: string): void { this.handlers.data.push(datas) };
+
+  onOpen(): void {
     console.log('[websokets: WebSocket.onOpen connection opened]');
-    let data: string = '';
+    let data: string | undefined = '';
     if (this.handlers.data.length > 0) {
-      data = this.handlers.data[0];
+      const handlers = this.handlers.data.slice(0);
+      data = handlers[0];
       // debugger
-      if (this.readyState === 1) {
+      if ((this.readyState as unknown as number) === 1) {
         console.log('[websokets: WebSocket.onOpen connection opened]');
         this.socket.send(data);
         this.handlers.data.pop();
-        return
-      } else setTimeout(() => this.onOpen(), 1000);
-    }
-    else if (this.readyState > 1) {
-      data = this.handlers.data[0];
+      } else setTimeout(() => { this.onOpen() }, 1000);
+    } else if ((this.readyState as unknown as number) > 1) {
+      const handlers = this.handlers.data.slice(0);
+      data = handlers[0];
       this.socket.send(data);
       this.handlers.data.pop();
-    }
-    else {
+    } else {
       console.error('[websokets: WebSocket.onOpen; Not datas for a Sehding]');
       this.handlers.data.pop();
     }
   };
 
-  get readyState() { return this.socket.readyState }
-  onMessage = (e: any) => { console.log('[websokets: WebSocket.onMessage - Received message]: ', e.data) };
-  onClose() { return this.socket.close() };
-  closing = (e: any) => { console.log('[websokets: WebSocket.closing - here is your handler]'); };
-  onError(e: any) { console.log('[websokets: WebSocket.onError - error]: ', e) };
+  get readyState(): () => typeof this.socket.readyState { return this.socket.readyState };
+
+  onMessage = (e): void => {
+    console.log('[websokets: WebSocket.onMessage - Received message]: ', e.data);
+  };
+
+  onClose(): void {
+    this.socket.close();
+  }
 }
 
 // WebSocets
