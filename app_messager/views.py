42f1ed20f7e62cb84ad073db0a507990832387f1
Tryd0g0lik <work80@mail.ru>
2024-04-08 11:22:08 +0700

    [1]: before install 'pip install django-webpack-loader==1.0.0' and settins

--- app_messager/views.py
+++ app_messager/views.py
@@ -5,9 +5,12 @@ import websocket, json
 
 # Create your views here.
 def home_page(request):
-	# User = get_user_model()
-	# user = User.objects.get(username="root")
-	# get_token(user)
+	User = get_user_model()
+	user = User.objects.all()
+	user = User.objects.get(username ='root')
+	# user_active = [u for u in user if u.is_active == True ]
+
+	get_token(user)
 	# ws = websocket.WebSocket()
 	# ws.connect('ws://localhost:6379/ws/tableData/')
-	return render(request, 'index.html', {})
+	return render(request, 'index.html', {'user_index': user.id})
