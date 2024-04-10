e4fbe3427d223f62981f8f3e2ff6d9b36ba12560
Tryd0g0lik <work80@mail.ru>
2024-04-10 11:06:36 +0700

    [1.1]: First commit. Models.py has been corrected. Before a migrate

--- app_messager/models.py
+++ app_messager/models.py
@@ -2,52 +2,55 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from uuid import uuid4
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 # from django.contrib.auth.models import AbstractUser, User, Group
-class TypeRequastion(models.TextChoices):
+class Messeger_TypeRequastionModel(models.TextChoices):
 	'''
-	TODO: Первый пользователь - автор заявки. Второй пользователь - тот, кто нажал кнопку перехода в диалог (мессенджер).
+	TODO: This's the first user. It's the order's (group's)  autor
 	'''
+	PARENTS = "A", _("Author")
+	CHILDE = "NA", _("NoAuthor")
 
-	PARENTS = "A", "Author"
-	CHILDE = "NA", "NoAuthor"
 
-class TypeSubRequastion(models.TextChoices):
-	'''
-	TODO: Статус пользователя
-	'''
-
-	PARENTS = "S", "Seller"
-	CHILDE = "B", "Buyer"
-
+class Messeger_SubTypeRequastionModel(models.TextChoices):
+  '''
+  TODO: This's the user's level
+  '''
+  PARENTS = "S", _("Seller")
+  CHILDE = "B", _("Buyer")
 
 User = get_user_model()
-class Group(models.Model):
-    '''The group model where multiple users can share and discuss ideas'''
-    uuid = models.UUIDField(default=uuid4, editable=False)
-    name = models.CharField(max_length=30)
-    members = models.ManyToManyField(User)
+class Messeger_GroupModel(models.Model):
+  '''The group model where multiple users can share and discuss ideas'''
+  uuid = models.UUIDField(default=uuid4, editable=False)
+  name = models.CharField(max_length=30)
+  members = models.ManyToManyField(User)
+  status = models.CharField(choices=Messeger_TypeRequastionModel, default=Messeger_TypeRequastionModel.PARENTS, max_length=10)
+  sub_status = models.CharField(choices=Messeger_SubTypeRequastionModel, default=Messeger_SubTypeRequastionModel.PARENTS, max_length=10)
+  title_order = models.CharField()
+  hide = models.BooleanField()
+  
+  def __str__(self) -> str:
+      return f"Group {self.name}-{self.uuid}"
 
-    def __str__(self) -> str:
-        return f"Group {self.name}-{self.uuid}"
+  def get_absolute_url(self):
+      return reverse("group", args=[str(self.uuid)])
 
-    def get_absolute_url(self):
-        return reverse("group", args=[str(self.uuid)])
+  def add_user_to_group(self, user:User):
+      '''A helper function to add a user to a group and create an event object'''
+      self.members.add(user)
+      self.event_set.create(type="Join", user=user)
+      self.save()
 
-    def add_user_to_group(self, user:User):
-        '''A helper function to add a user to a group and create an event object'''
-        self.members.add(user)
-        self.event_set.create(type="Join", user=user)
-        self.save()
-
-    def remove_user_from_group(self, user:User):
-        '''An helper function to remove users from group members when they \
-        leave the group and create an event for the timestamp the user left the group'''
-        self.members.remove(user)
-        self.event_set.create(type="Left", user=user)
-        self.save()
+  def remove_user_from_group(self, user:User):
+      '''An helper function to remove users from group members when they \
+      leave the group and create an event for the timestamp the user left the group'''
+      self.members.remove(user)
+      self.event_set.create(type="Left", user=user)
+      self.save()
 
 
-class Message(models.Model):
+class Messeger_MessageModel(models.Model):
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	content = models.TextField()
@@ -58,25 +61,30 @@ class Message(models.Model):
 		time = self.timestamp.time()
 		return f"{self.author}:- {self.content} @{date} {time.hour}:{time.minute}"
 
-class Event(models.Model):
-    '''
-    A model that holds all events related to a group like when a user joins the group or leaves.
-    '''
-    CHOICES = [
-        ("Join", "join"),
-        ("Left", "left")
-        ]
-    type = models.CharField(choices=CHOICES, max_length=10)
-    description= models.CharField(help_text="A description of the event that occurred",\
-    max_length=50, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    group = models.ForeignKey(Group ,on_delete=models.CASCADE)
 
-    def save(self, *args, **kwargs):
-        self.description = f"{self.user} {self.type} the {self.group.name} group"
-        super().save(*args, kwargs)
+class Messeger_FilesModels(models.Model):
+	link = models.CharField(verbose_name="File path name")
+	size = models.FloatField()
 
-    def __str__(self) -> str:
-        return f"{self.description}"
+class Messeger_EventModels(models.Model):
+  '''
+  A model that holds all events related to a group like when a user joins the group or leaves.
+  '''
+  group = models.ForeignKey(Group ,on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  CHOICES = [
+      ("Join", "join"),
+      ("Left", "left")
+      ]
+  type = models.CharField(choices=CHOICES, max_length=10)
+  description= models.CharField(help_text="A description of the event that occurred",
+  max_length=50, editable=False)
+  timestamp = models.DateTimeField(auto_now_add=True)
+
+  def save(self, *args, **kwargs):
+      self.description = f"{self.user} {self.type} the {self.group.name} group"
+      super().save(*args, kwargs)
+
+  def __str__(self) -> str:
+      return f"{self.description}"
 
