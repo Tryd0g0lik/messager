20f63af6f10bf7dd20953c1ed1c2790dc673d8dd
Tryd0g0lik <work80@mail.ru>
2024-04-21 13:50:58 +0700

    [2]: added 3/3 added the event listener to the input form That 'handlerGetMessageOfInput'

--- app_messager/frontend/src/scripts/services/handlers/messages/old-message/sendler-old-message.ts
+++ app_messager/frontend/src/scripts/services/handlers/messages/old-message/sendler-old-message.ts
@@ -2,17 +2,20 @@
 import cleanForm from '@Service/cleaner';
 import handlerSendlerMessageTotal from '../sendler_message';
 import changeOldPost from '../sub-handler/subhandler-old-post';
+import handlerGetMessageOfInput from '../get-messages';
 
 const sendlerOldMessageTotal = (): (e: KeyboardEvent | MouseEvent) => void => {
   const rewriteContentAsy = async (e: KeyboardEvent | MouseEvent): Promise<void> => {
     await changeOldPost(e);
 
     handlerSendlerMessageTotal(true);
-    cleanForm();
   };
 
   return (e: KeyboardEvent | MouseEvent): void => {
     rewriteContentAsy(e);
+    cleanForm();
+    /* 3/3 added the event listener to the input form . It is change of the listener */
+    handlerGetMessageOfInput(handlerSendlerMessageTotal());
   };
 };
 
