ba5ad09d90546ee3cbe33f2ff2abeadc6277cb4e
Tryd0g0lik <work80@mail.ru>
2024-05-12 13:49:40 +0700

    [3.2]: that added the propt search

--- app_messager/frontend/src/scripts/searcher/index.ts
+++ app_messager/frontend/src/scripts/searcher/index.ts
@@ -1,11 +1,12 @@
 // app_messager\frontend\src\scripts\services\searcher\index.ts
 
-import { Searher } from './oop/search';
+// import { Searher } from './oop/search';
+import { LiveSearcher } from './oop/live-search';
 import './style.css';
 /* ------ Search ------ */
 document.addEventListener('DOMContentLoaded', () => {
   const topsearch = document.querySelector('#searchup');
   if (topsearch !== null) {
-    const search = new Searher(topsearch as HTMLDivElement);
+    const search = new LiveSearcher(topsearch as HTMLDivElement);
   };
 });
--- app_messager/frontend/src/scripts/searcher/oop/live-search.ts
+++ app_messager/frontend/src/scripts/searcher/oop/live-search.ts
@@ -1,3 +1,5 @@
+import { ChatMessage } from '@Interfaces';
+import fun from '../functions';
 import { Searher } from './search';
 
 /**
@@ -8,9 +10,105 @@ import { Searher } from './search';
  */
 export class LiveSearcher extends Searher {
   result = [];
+
+  words: string
   constructor(element: HTMLDivElement | HTMLFormElement) {
     super(element);
     this.result = [];
+    this.words = '';
+
+    const htmlElement = fun.detectionElement('input[type="search"]');
+    if (htmlElement === null) {
+      this.errors('[LiveSearcher > htmlElement]');
+    }
+    const handlerKeyBoard = this.handlerKeyBoard.bind(this);
+    (htmlElement as HTMLInputElement).removeEventListener('keyup', handlerKeyBoard);
+    (htmlElement as HTMLInputElement).addEventListener('keyup', handlerKeyBoard);
+  }
+
+  async handlerKeyBoard(e: KeyboardEvent): Promise<void> {
+    const target = e.target as HTMLElement;
+    if ((e.type.includes('keyup') && (!(e.key).includes('Enter'))) && ((target.tagName).includes('INPUT'))) {
+      this.words = (target as HTMLInputElement).value;
+      if (this.words.length < 3) {
+        this.clear('.prompting');
+        return;
+      }
+
+      const searcherObj = await this.receive(this.words);
+      const templateresult = this.templateresult.bind(this);
+      if (searcherObj === undefined) {
+        this.errors('[LiveSearcher > handlerKeyBoard > searcherObj]');
+      }
+
+      let htmlLi = '';
+      await ((searcherObj.searcher) as ChatMessage[]).forEach((item) => {
+        htmlLi += String(templateresult(item));
+      });
+
+      if (htmlLi.length === 0) {
+        this.errors('[LiveSearcher >handlerKeyBoard > searcherObj.searcher]');
+      }
+
+      this.clear('.prompting');
+      const div = document.createElement('div');
+      div.className = 'prompting';
+      div.innerHTML = `<ul>${htmlLi}</ul>`;
+      const res = fun.detectionElement('#searchup');
+      const location = fun.htmlBoxLocation('input[type="search"]');
+      if (location !== undefined) {
+        div.style.left = String(location.left) + 'px';
+      }
+      if (res !== null) {
+        res.insertAdjacentHTML('afterend', div.outerHTML);
+      }
+    } else if (((e.key).includes('Escape')) || ((e.key).includes('Enter'))) {
+      debugger
+      this.clear('.prompting');
+    }
+  }
+
+  private async receive(v: string): Promise<Record<'searcher', object>> {
+    const url = new URL('api/v1/search/get/', 'http://127.0.0.1:8000/');
+    url.searchParams.set('searcher', v);
+    this.urls = url;
+    const response = await this.get({ contentType: 'application/json' });
+    return response as Record<'searcher', object>;
+  };
+
+  private templateresult(prop: ChatMessage): string | undefined {
+    const { authorId, message, postId } = prop;
+    const wordArr = message.split(' ');
+    const swords = this.words;
+    const wordFilt = wordArr.filter((item) => item.includes(swords));
+    if (wordFilt.length === 0) {
+      return;
+    }
+
+    const li = document.createElement('li');
+    li.innerHTML = `<span>${wordFilt[0]}</span><span><a href="/#${postId}">mess</a></span>`;
+    return li.outerHTML;
+  }
+
+  clear(select: string): void {
+    const res = fun.detectionElement(select);
+    if (res !== null) {
+      res.remove();
+    }
+  }
+
+  /**
+   *
+   * @param `name` type a string. Exemple is '[handlerKeyBoard > searcherObj.searcher]'
+   * @returns void
+   */
+  errors(name: string): void {
+    const err = new Error('Something what wrong!');
+    err.name = name;
+    throw err;
   }
 
 }
+
+// const { searcher } = datas;
+// datas: Record<'searcher', object>
