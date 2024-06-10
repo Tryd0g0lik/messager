5f11da0c905bdde550160dea62e83ce2b8d33eba
Tryd0g0lik <work80@mail.ru>
2024-06-10 12:08:22 +0700

    [4]: greated the new migration. It's under number 002

--- app_messager/migrations/0001_initial.py
+++ app_messager/migrations/0001_initial.py
@@ -11,11 +11,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Messages',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('id_users', models.IntegerField()),
-            ],
-        ),
+
     ]
