a7d510d3d8a62d2e445953a202a561bc9514d1b0
Tryd0g0lik <work80@mail.ru>
2024-06-20 05:56:44 +0700

    [master]: pause

--- project/rest_routers.py
+++ project/rest_routers.py
@@ -3,13 +3,8 @@ from rest_framework.routers import DefaultRouter
 from app_messager import views
 
 router = DefaultRouter()
-# router.register(r'chat/make/post/', views.PostAPIFilterViews.as_view(), basename='postchat')
 router.register(r'get', views.PostAPIFilterViews, basename='message_get')
 router.register(r'update', views.MessageUpdateViews, basename='message_update')
-
-# router.register(r'chat/upload/files/', views.upload_file, basename="upload_file")
-# router.register(r'chat/update/<int:pk>/', views.UpdateMessages.as_view(), basename='updatemessageid')# update the one post # , name="requests_messages" # (([0-9]{2,4}|[-]){1,5}_[0-9:\.]{2,13})[0-9]{1,2}$'
-# router.register(r'chat/delete/files/', views.PostAPIDeleteFilelView.as_view(), basename='chatfiles')
 
 # <QueryDict: {'author': ['3'], 'content': ['ds-88\r\n'], 'group': ['1'], 'file': ['']}>
 
