a788539a123f6f6cd225e059fd0cb0ebd44c5a0f
Tryd0g0lik <work80@mail.ru>
2024-06-20 05:44:10 +0700

    [4.1]: corrected

--- project/rest_routers.py
+++ project/rest_routers.py
@@ -15,3 +15,4 @@ router.register(r'update', views.MessageUpdateViews, basename='message_update')
 
 
 # initial_data: {'corrects': False, 'eventtime': '2024-6-15@4:53:24 PM', 'message': 'ddds-66 -33', 'userId': '3', 'groupId': '7a3a744a-64ab-492b-89bf-9ee7c72b91f1'}
+
